347b1b7ed5773120e65a4e4126dfe2f95d81ab2f
SVSathwick <sathwick.sv@gmail.com>
2022-11-13 10:31:03 +0530

    999 Added functionality to read soil sensor config files

--- Soil_Sensor_AWS_Publish/src/owm.py
+++ Soil_Sensor_AWS_Publish/src/owm.py
@@ -12,8 +12,15 @@ from pyowm import OWM
 import json
 import pprint
 
-owm = OWM('0f8b321c68552dff33eeb5625f971c39')
-mgr = owm.weather_manager()
-one_call = mgr.one_call(lat=28.5355, lon=77.3910)
-current_data = json.dumps(one_call.current.__dict__)
-print(current_data)
+for i in range(10):
+    owm = OWM('0f8b321c68552dff33eeb5625f971c39')
+    mgr = owm.weather_manager()
+    one_call = mgr.one_call(lat=28.5355, lon=77.3910)
+    current_data = json.dumps(one_call.current.__dict__)
+
+    humidity = current_data['humidity']
+    temp_master = current_data['temp']
+    temp = temp_master['temp']
+    feels_like = temp_master['feels_like']
+
+    print(f'Humidity: {humidity}, Temp: {temp}, Feels_like: {feels_like}')
--- Soil_Sensor_AWS_Publish/src/soil_sensor_publish.py
+++ Soil_Sensor_AWS_Publish/src/soil_sensor_publish.py
@@ -16,9 +16,12 @@ class AWS():
     # This method will obviosuly be called while creating the instance
     # It will create the MQTT client for AWS using the credentials
     # Connect operation will make sure that connection is established between the device and AWS MQTT
-    def __init__(self, client, certificate, private_key):
+    def __init__(self, client, lon, lat, sprinkler, certificate, private_key):
         self.client_id = client
-        self.device_id = client
+        self.device_id = client       
+        self.lat = lat
+        self.lon = lon
+        self.sprinkler = sprinkler
         self.cert_path = PATH_TO_CERT + "\\" + certificate
         self.pvt_key_path = PATH_TO_CERT + "\\" + private_key
         self.root_path = PATH_TO_CERT + "\\" + "AmazonRootCA1.pem"
@@ -42,6 +45,9 @@ class AWS():
             timestamp = str(datetime.datetime.now())
             message['deviceid'] = self.device_id
             message['timestamp'] = timestamp
+            message['lat'] = self.lat
+            message['lon'] = self.lon            
+            message['sprinkler'] = self.sprinkler
             message['datatype'] = 'Temperature'
             message['value'] = value
             messageJson = json.dumps(message)
@@ -59,13 +65,41 @@ class AWS():
 # Also there can be different method calls as well based on the devices and their working.
 if __name__ == '__main__':
     # SOil sensor device Objects
-    soil_sensor_1 = AWS("soil_sensor_1", "DeviceCertificate-certificate.pem.crt", "BSM_G101-private.pem.key")
-    soil_sensor_2 = AWS("soil_sensor_2", "DeviceCertificate-certificate.pem.crt", "BSM_G101-private.pem.key")
-    soil_sensor_3 = AWS("soil_sensor_3", "DeviceCertificate-certificate.pem.crt", "BSM_G101-private.pem.key")
-    soil_sensor_4 = AWS("soil_sensor_4", "DeviceCertificate-certificate.pem.crt", "BSM_G101-private.pem.key")
-    soil_sensor_5 = AWS("soil_sensor_5", "DeviceCertificate-certificate.pem.crt", "BSM_G101-private.pem.key")
+    # soil_sensor_1 = AWS("soil_sensor_1", "DeviceCertificate-certificate.pem.crt", "BSM_G101-private.pem.key")
+    # soil_sensor_2 = AWS("soil_sensor_2", "DeviceCertificate-certificate.pem.crt", "BSM_G101-private.pem.key")
+    # soil_sensor_3 = AWS("soil_sensor_3", "DeviceCertificate-certificate.pem.crt", "BSM_G101-private.pem.key")
+    # soil_sensor_4 = AWS("soil_sensor_4", "DeviceCertificate-certificate.pem.crt", "BSM_G101-private.pem.key")
+    # soil_sensor_5 = AWS("soil_sensor_5", "DeviceCertificate-certificate.pem.crt", "BSM_G101-private.pem.key")
 
-    soil_sensors = [soil_sensor_1, soil_sensor_2, soil_sensor_3, soil_sensor_4, soil_sensor_5]
+    # soil_sensors = [soil_sensor_1, soil_sensor_2, soil_sensor_3, soil_sensor_4, soil_sensor_5]
 
-    for sensor in soil_sensors:
-        sensor.publish()
+    # for sensor in soil_sensors:
+    #     sensor.publish()
+
+    #Reading the configuration file
+    f = open('sprinkler_config.json', 'r')
+    config = json.loads(f.read())
+    f.close()
+
+    sensors = []
+
+    sprinklers = config['sprinklers']
+    for sprinkler in sprinklers:
+        # print(f'Sprinkler: ', sprinkler['name'])
+        
+        sensors_sp = sprinkler['soil_sensors']
+        for sensor in sensors_sp:
+            # print('')
+            # print('\tdevice_id: ', sensor['device_id'])
+            # print('\tlat: ', sensor['lat'])
+            # print('\tlon: ', sensor['lon'])
+            # print('')
+            sensor['sprinkler'] = sprinkler['name']
+            sensors.append(sensor)
+        # print(sprinkler)
+
+    # print(config)
+
+    for sensor in sensors:
+        print(f"Device id: {sensor['device_id']}, Lattitude: {sensor['lat']}, Longitude: {sensor['lon']}, Sprinkler: {sensor['sprinkler']}")
+        print(f"Certificate: {sensor['certificate']}, Private Key: {sensor['private_key']}\n")
